cbc3bc5719635dec6dee0a6d4e1f42d68a0f68e4
Kshitij Singh <kshitij1118@gmail.com>
2025-03-06 12:11:40 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "first-ermine-239.convex.cloud",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 };
